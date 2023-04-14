9755bf3a51cf7eb18a67af8dfe7d3a4a2ed98563
Lucas <lucas.g.oliveira1998@outlook.com>
2023-04-14 20:59:27 -0300

    Fazendo mais um commit com o SVN.

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -2,6 +2,14 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		int idade = 25;
+	
+		if (idade > 18) {
+			System.out.println("Você é maior de idade!");
+		} else {
+			System.out.println("Você é menor de idade!");
+		}
+		
 		System.out.println("Hello World!");
 	}
 }
