45638afa4bdd4db6c11cb0f093f01b86a1030b85
Lucas <lucas.g.oliveira1998@outlook.com>
2023-04-14 21:05:01 -0300

    Adicionando mais um if

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -2,10 +2,12 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int idade = 25;
+		int idade = 70;
 	
 		if (idade > 18) {
 			System.out.println("Você é maior de idade!");
+		} else if (idade > 65) {
+			System.out.println("Você já pode se aposentar!");
 		} else {
 			System.out.println("Você é menor de idade!");
 		}
