b5e0fc1ad511d1b2e7a841c7a3ab652d1d6dbad5
Lucas <lucas.g.oliveira1998@outlook.com>
2023-04-14 21:14:13 -0300

    Resolvendo conflitos de versões distintas.

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -2,12 +2,14 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int idade = 70;
+		int idade = 8;
 	
-		if (idade > 18) {
-			System.out.println("Você é maior de idade!");
-		} else if (idade > 65) {
+		if (idade > 65) {
 			System.out.println("Você já pode se aposentar!");
+		} else if (idade > 18) {
+			System.out.println("Você é maior de idade!");
+		} else if (idade < 10) {
+			System.out.println("Você tem menos de 10 anos de idade!");
 		} else {
 			System.out.println("Você é menor de idade!");
 		}
